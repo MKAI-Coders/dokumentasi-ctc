f6379d0e8fdf09fed22756e4289ca0ea3093e592
spdin <saripudin.nd@gmail.com>
2019-12-27 16:54:56 +0700

    mobile view

--- resources/views/form_upload.blade.php
+++ resources/views/form_upload.blade.php
@@ -1,5 +1,10 @@
+<!DOCTYPE html>
 <html lang="en">
 <head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  
   <title>DOKUMENTASI FOTO CTC 2020</title>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
